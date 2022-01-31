09dc5a5da6259a4ee5b88d76af4e8de3e881881f
Kalingga Padel Muhamad <enginerbros@gmail.com>
2022-01-31 16:28:44 +0700

    Update Lesson 2 dataType

---
+++ 2. dataType/dataType.php
@@ -0,0 +1,29 @@
+<?php
+
+/* 
+dataType in php
+String, int, floatj, bolean and many more
+*/
+
+//example
+
+//string
+$studentName = "Kalingga Padel Muhamad";
+
+//int
+$studentAge = 19;
+
+//float
+$studentWeight = 50.5;
+
+//bolean
+$isSmart = true;
+
+
+echo "<p> Student Name   : $studentName </p>";
+echo "<p> Student Age    : $studentAge </p>";
+echo "<p> Student Weight : $studentWeight </p>";
+echo "<p> Student Smart  : $isSmart </p>";
+
+
+?>
