1ebbef1f95409b72906e08c1f12a1d7d6a7ae73c
Kalingga Padel Muhamad <enginerbros@gmail.com>
2022-02-01 14:46:11 +0700

    Lesson 3 arithmetic ini PHP

---
+++ 3. arithmetic/arithmetic.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+arithmetic operation ini php
++,*,/,-
+*/
+
+// example
+
+//add
+echo 1+1;
+
+//multiple
+echo 1*2;
+
+//distribution
+echo 10/3;
+
+//subtraction
+echo 10-7;
+
+?>
---
+++ 3. arithmetic/arithmeticUsingVariable.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+arithmetic operation ini php using variable
++,*,/,-
+*/
+
+// example
+
+//Deklaration variable
+$number1 = 10;
+$number2 = 30;
+
+//add
+echo "<p>". $number1 + $number2. "</p>";
+
+//multiple
+echo "<p>". $number1 * $number2. "</p>";
+
+//distribution
+echo "<p>". $number1 / $number2. "</p>";
+
+//subtraction
+echo "<p>". $number1 - $number2. "</p>";
+
+/*
+or you can save result of operation in variable like
+$storage = $number1 + $number2;
+echo $storage
+*/
+
+
+?>
