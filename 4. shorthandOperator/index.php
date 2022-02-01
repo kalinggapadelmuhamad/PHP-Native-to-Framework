72e98ff834f6b21dca0d0766bd91c18e80e7233c
Kalingga Padel Muhamad <enginerbros@gmail.com>
2022-02-01 15:01:02 +0700

    Lesson 4 shorthandOperator

---
+++ 4. shorthandOperator/index.php
@@ -0,0 +1,15 @@
+<?php
+
+/*
+shorthandOperator can make your code more efficient dan clean 
+let see how it works
+*/
+
+//example
+$number1 = 10;
+$number2 = 20;
+
+$number1 += $number2;
+//the above code has the same function  as $number1 = $number1 + $number2
+
+?>
